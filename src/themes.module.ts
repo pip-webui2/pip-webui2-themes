0234bdb1ae0bfba05fc15ce726685a239b36f437
Kate Negrienko <katya@negrienko.com>
2017-10-11 18:52:38 +0300

    [Module] add module for themes service

---
+++ src/themes.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { PipThemesService } from './shared/themes.service';
+
+@NgModule({
+    declarations: [],
+    imports: [],
+    exports: [],
+    providers: [
+        PipThemesService
+    ]
+})
+export class PipThemesModule { }
