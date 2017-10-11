8fd99fbb93cc751dd436ad72cf239e11aaa4bb71
Kate Negrienko <katya@negrienko.com>
2017-10-11 18:52:00 +0300

    [Service] add service of themes

---
+++ src/shared/themes.service.ts
@@ -0,0 +1,49 @@
+import * as _ from 'lodash';
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+@Injectable()
+export class PipThemesService {
+
+    private _localTheme:  string;
+    private _arrayThemes: string[] = ["candy-theme", "unicorn-dark-theme"];
+    private _theme$: BehaviorSubject<string>;
+
+    public constructor() {
+        console.log(window.localStorage.getItem('theme'));
+        this._localTheme = window.localStorage.getItem('theme') || this._arrayThemes[0];
+        this._theme$ = new BehaviorSubject<string>(this._localTheme);
+        this.useTheme(this._localTheme);
+    }
+
+    public get themes(): string[] {
+        return this._arrayThemes;
+    }
+
+    public get theme(): string {
+        return this._localTheme;
+    }
+
+    public get theme$(): Observable<string> {
+        return this._theme$;
+    }
+    
+    public useTheme(theme: string) {
+
+        window.localStorage.setItem('theme', theme);
+
+        document.body.classList.remove(this._localTheme);
+        this._localTheme = theme;
+        this._theme$.next(this._localTheme);
+        document.body.classList.add(theme);
+        
+    }
+
+}
