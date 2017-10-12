f8b3eb6d35d692f61a1d1d5d01bc3161107ba854
Kate Negrienko <katya@negrienko.com>
2017-10-12 18:47:51 +0300

    [Themes] add pink themes

--- src/shared/themes.service.ts
+++ src/shared/themes.service.ts
@@ -13,7 +13,7 @@ import 'rxjs/add/operator/catch';
 export class PipThemesService {
 
     private _localTheme: string;
-    private _arrayThemes: string[] = ["candy-theme", "unicorn-dark-theme", "pip-blue-theme", "pip-green-theme"];
+    private _arrayThemes: string[] = ["candy-theme", "unicorn-dark-theme", "pip-blue-theme", "pip-green-theme", "pip-pink-theme"];
     private _theme$: BehaviorSubject<string>;
 
     public constructor() {
