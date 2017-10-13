26b7f8e697571e0522ab71929f15686470850cb5
Kate Negrienko <katya@negrienko.com>
2017-10-13 13:55:29 +0300

    [Themes def] add black theme

--- src/shared/themes.service.ts
+++ src/shared/themes.service.ts
@@ -14,7 +14,7 @@ export class PipThemesService {
 
     private _localTheme: string;
     private _arrayThemes: string[] = ["candy-theme", "unicorn-dark-theme", "pip-blue-theme", "pip-grey-theme",
-    "pip-navy-theme", "pip-amber-theme",  "pip-green-theme", "pip-orange-theme", "pip-pink-theme"];
+    "pip-navy-theme", "pip-amber-theme",  "pip-green-theme", "pip-orange-theme", "pip-pink-theme", "pip-dark-theme", "pip-black-theme"];
     private _theme$: BehaviorSubject<string>;
 
     public constructor() {
