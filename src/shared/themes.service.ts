883d0f09f780c97ef87d73caba9a8c2bb22f7ac4
KateNegrienko <katya@negrienko.com>
2017-11-01 17:54:29 +0200

    [Theme service]change theme service

--- src/shared/themes.service.ts
+++ src/shared/themes.service.ts
@@ -10,64 +10,50 @@ export const defaultPalette: string = 'light';
 export class PipThemesService {
 
     private _selectedTheme: ThemeModel = new ThemeModel();
-    private _themes: ThemeModel[] = [{
-        name: "candy-theme",
-        palette: 'light'
-    }, {
-        name: "unicorn-dark-theme",
-        palette: 'dark'
-    },
-    {
-        name: "pip-blue-theme",
-        palette: 'light'
-    },
-
-    {
-        name: "pip-grey-theme",
-        palette: 'light'
-    },
-    {
-        name: "pip-navy-theme",
-        palette: 'light'
-    },
-    {
-        name: "pip-amber-theme",
-        palette: 'light'
-    },
-
-    {
-        name: "pip-green-theme",
-        palette: 'light'
-    },
-
-    {
-        name: "pip-orange-theme",
-        palette: 'light'
-    },
-
-    {
-        name: "pip-pink-theme",
-        palette: 'light'
-    },
-    {
-        name: "pip-dark-theme",
-        palette: 'dark'
-    },
-
-    {
-        name: "pip-black-theme",
-        palette: 'dark'
-    },
-    {
-        name: "bootbarn-warm-theme",
-        palette: 'light'
-    }, {
-        name: "bootbarn-cool-theme",
-        palette: 'light'
-    }, {
-        name: "bootbarn-mono-theme",
-        palette: 'light'
-    }
+    private _themes: ThemeModel[] = [
+        {
+            name: "candy-theme",
+            palette: 'light'
+        }, {
+            name: "unicorn-dark-theme",
+            palette: 'dark'
+        }, {
+            name: "pip-blue-theme",
+            palette: 'light'
+        }, {
+            name: "pip-grey-theme",
+            palette: 'light'
+        }, {
+            name: "pip-navy-theme",
+            palette: 'light'
+        }, {
+            name: "pip-amber-theme",
+            palette: 'light'
+        }, {
+            name: "pip-green-theme",
+            palette: 'light'
+        }, {
+            name: "pip-orange-theme",
+            palette: 'light'
+        }, {
+            name: "pip-pink-theme",
+            palette: 'light'
+        }, {
+            name: "pip-dark-theme",
+            palette: 'dark'
+        }, {
+            name: "pip-black-theme",
+            palette: 'dark'
+        }, {
+            name: "bootbarn-warm-theme",
+            palette: 'light'
+        }, {
+            name: "bootbarn-cool-theme",
+            palette: 'light'
+        }, {
+            name: "bootbarn-mono-theme",
+            palette: 'light'
+        }
     ];
 
     private _themes$ = new BehaviorSubject<ThemeModel[]>(this._themes);
@@ -76,7 +62,7 @@ export class PipThemesService {
     public constructor() {
         let theme: string = window.localStorage.getItem('theme');
         if (theme) {
-            this.selectedTheme = this._themes.find((item) => {return item.name == theme}) || { name: theme, palette: defaultPalette };
+            this.selectedTheme = this._themes.find((item) => { return item.name == theme }) || { name: theme, palette: defaultPalette };
         } else {
             this.selectedTheme = { name: defaultTheme, palette: defaultPalette };
         }
