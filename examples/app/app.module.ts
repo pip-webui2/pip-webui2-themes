dda2e68c8f414b8622f98f8b544d6a3319caa27f
KateNegrienko <katya@negrienko.com>
2017-11-02 19:09:34 +0200

    [Samples] add icons to samples in themes

--- examples/app/app.module.ts
+++ examples/app/app.module.ts
@@ -8,7 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 
 
-import { MatButton, MatButtonModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
+import {  MatIconModule, MatButton, MatButtonModule, MatToolbarModule, MatSidenavModule } from '@angular/material';
 import { MatSelectModule } from '@angular/material';
 import { PipThemesModule } from './pip-webui2-themes';
 
@@ -23,6 +23,7 @@ import { PipThemesModule } from './pip-webui2-themes';
     FormsModule,
     MatButtonModule,
     MatSelectModule,
+    MatIconModule,
     MatToolbarModule,
     PipThemesModule,
     BrowserAnimationsModule,
